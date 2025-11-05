1d7ea325bbd8e3bb51fbe3c9a5e619d22eae507c
osvaldecir viana <manocatanduva@gmail.com>
2025-11-05 11:21:20 -0300

    //

---
+++ scripts.js
@@ -0,0 +1,17 @@
+const prevButton = document.getElementById('prev')
+const nexButton = document.getElementById('next')
+const item = document.querySelectorAll('.item')
+const dots = document.querySelectorAll('.dot')
+const numberIndicator = document.querySelector('.number')
+const list = document.querySelector('.list')
+
+
+
+let active = 0;
+const total = item.length
+let timer;
+
+
+
+
+
